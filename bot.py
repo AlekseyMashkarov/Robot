4a4e000b2c3020cfaa331698d44c8995699369dc
AlekseyMashkarov <kash512@yandex.ru>
2020-07-04 14:48:42 +0300

    Добавил анкету

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 # Импорт компонентов
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters
 from settings import TG_TOKEN, TG_API_URL
-from handlers import sms, get_anecdote, get_location, get_contact, parrot
-
+from handlers import *
 
 
 
@@ -16,6 +15,18 @@ def main():
     my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Анекдот'), get_anecdote)) # обрабатываем текст кнопки
     my_bot.dispatcher.add_handler(MessageHandler(Filters.contact, get_contact)) #Обработчик полученных контактов
     my_bot.dispatcher.add_handler(MessageHandler(Filters.location, get_location))  # Обработчик полученных геоданных
+    my_bot.dispatcher.add_handler(ConversationHandler(entry_points=[MessageHandler(Filters.regex('Заполнить анкету'), anketa_start)],
+                                                      states={
+                                                          "user_name": [MessageHandler(Filters.text, anketa_get_name)],
+                                                          "user_age": [MessageHandler(Filters.text, anketa_get_age)],
+                                                          "evaluation": [MessageHandler(Filters.regex('1|2|3|4|5'), anketa_get_evaluation)],
+                                                          "comment": [MessageHandler(Filters.regex('Пропустить'), anketa_exit_comment),
+                                                                      MessageHandler(Filters.text, anketa_comment)]
+                                                      },
+                                                      fallbacks=[MessageHandler(
+                                                          Filters.text | Filters.video | Filters.photo | Filters.document, dontknow)]
+                                                      )
+                                  )
     my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot)) #Обработчик текстового сообщения
 
     my_bot.start_polling() # Проверка наличия сообщений платформы
--- utility.py
+++ utility.py
@@ -6,5 +6,7 @@ def get_keyboard():
     contact_button = KeyboardButton('Отправить контакты', request_contact=True)
     location_button = KeyboardButton('Отправить геопозицию', request_location=True)
     my_keyboard = ReplyKeyboardMarkup([['Анекдот'], ['Начать'],
-                                      [contact_button, location_button]], resize_keyboard=True)  # Добавлена кнопка
+                                      [contact_button, location_button],
+                                       ['Заполнить анкету']
+                                       ], resize_keyboard=True)  # Добавлена кнопка
     return my_keyboard
